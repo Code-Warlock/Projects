83e1d385c2259c1919baeabde538cf91d77d93e5
Code-Warlock <dessymichaeltgp123@gmail.com>
2021-10-28 14:39:00 +0100

    Update Files

--- basic_spider.py
+++ basic_spider.py
@@ -1,15 +1,18 @@
+""" Crawler Program """
 import requests
 from bs4 import BeautifulSoup
 def main_spider(url):
+    """Main Spider Func """
     urls = []
     request = requests.get(url)
     web_text = request.text
     soup = BeautifulSoup(str(web_text))
     for card in soup.find_all("a",{"class" : "dropdown-item"}):
-        x = card.get("href")
-        urls.append(url + x)
+        href = card.get("href")
+        urls.append(url + href)
         return urls
 def each_spider(*urls):
+    """URL Getting function """
     for each_link in urls:
         request = requests.get(each_link)
         link_text = request.text
@@ -18,6 +21,7 @@ def each_spider(*urls):
             each_link = each_link.replace("products.html#shirts","")
             print(each_link+image_address.get("src"))
 def runner():
+    """Runner func """
     values = main_spider("https://a1-uniforms.netlify.app/")
     each_spider(*values)
 runner()
